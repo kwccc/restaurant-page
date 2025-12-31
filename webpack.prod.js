63a89118b32700abbceb1785f3a0036f53e8e03b
Kai Cheung <kaicheunga@gmail.com>
2025-12-31 18:19:37 +0000

    Split webpack config to dev and prod with webpack merge

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,29 +1,6 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
 
-module.exports = {
+module.exports = merge(common, {
   mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(png|jpg|jpeg)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+});
